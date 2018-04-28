dbcc6dc01e44c25c3087716ce0aedd17809e3516
Aerex <nanaus13@yahoo.com>
2018-04-27 23:49:27 -0500

    fix(webpack): add environment mode in configuration

--- generators/app/templates/react/webpack.prod.js
+++ generators/app/templates/react/webpack.prod.js
@@ -17,13 +17,11 @@ const UglifyJSPlugin = require('uglifyjs-webpack-plugin');
 const common = require('./webpack.common');
 
 module.exports = merge(common, {
-  devtool: 'source-map',
-  plugins: [
-    new UglifyJSPlugin({
-      sourceMap: true
-    }),
-    new webpack.DefinePlugin({
-      'process.env.NODE_ENV': '"production"'
-    })
-  ]
+	devtool: 'source-map',
+	mode: 'production',
+	plugins: [
+		new UglifyJSPlugin({
+			sourceMap: true
+		})
+	]
 });
